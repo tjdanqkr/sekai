fb7955d0fe1c3b94e1c1f63c6c9f9c783ccf162e
tjdanqkr <49061542+tjdanqkr@users.noreply.github.com>
2019-05-07 20:57:07 +0900

    ddd

--- Sinseikai/src/net/cus/db/cusdao.java
+++ Sinseikai/src/net/cus/db/cusdao.java
@@ -13,56 +13,98 @@ import javax.sql.DataSource;
 
 import net.db.DAO;
 
-
-public class cusdao implements DAO{
+public class cusdao implements DAO {
 	Connection con;
 	PreparedStatement pstmt;
 	ResultSet rs;
-	
+
 	public cusdao() {
-		try{
+		try {
 			Context init = new InitialContext();
-	  		DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
-	  		con = ds.getConnection();
-		}catch(Exception ex){
+			DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+			con = ds.getConnection();
+		} catch (Exception ex) {
 			System.out.println("DB ���� ���� : " + ex);
 			return;
 		}
 	}
-	
+
 	public List getList(cusbean bean) {
-		
+
 		List list = new ArrayList();
-		try{
-			pstmt=con.prepareStatement("select * from cusboard where name=?");
+		try {
+			pstmt = con.prepareStatement("select * from cusboard where name=?");
 			pstmt.setString(1, bean.getName());
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				
 				bean.setTitle(rs.getString("title"));
 				bean.setCon(rs.getString("con"));
 				list.add(bean);
+
 			}
 			return list;
-		}catch(Exception ex){
-			System.out.println("getListCount ����: " + ex);			
-		}finally{
-			if(rs!=null) try{rs.close();}catch(SQLException ex){}
-			if(pstmt!=null) try{pstmt.close();}catch(SQLException ex){}
+		} catch (Exception ex) {
+			System.out.println("getListCount ����: " + ex);
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+				}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+				}
 		}
 		return null;
+
+	}
+
+	public cusbean Detail(cusbean bean) {
+
 		
+		try {
+			pstmt = con.prepareStatement("select * from cusboard where title=?");
+			pstmt.setString(1, bean.getTitle());
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				
+				bean.setTitle(rs.getString("title"));
+				bean.setCon(rs.getString("con"));
+				
+
+			}
+			return bean;
+		} catch (Exception ex) {
+			System.out.println("getListCount ����: " + ex);
+		} finally {
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+				}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+				}
+		}
+		return null;
+
 	}
 
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
-		if(con != null) {
+		if (con != null) {
 			try {
 				con.close();
 			} catch (SQLException e) {
-			
+
 				e.printStackTrace();
 			}
 		}
