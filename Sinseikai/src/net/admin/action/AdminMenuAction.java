554b141d5b11f946f863373989f7a6a2ca5658b2
Kyuhyeon Kim <40159616+TrianglePoint@users.noreply.github.com>
2019-05-07 12:38:22 +0900

    관리자 메뉴와 overview Action 생성 (틀만 있음)
     - overview는 다른 상세 보기가 완료된 후 추가할 예정

---
+++ Sinseikai/src/net/admin/action/AdminMenuAction.java
@@ -0,0 +1,16 @@
+package net.admin.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.action.Action;
+import net.action.ActionForward;
+
+public class AdminMenuAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+	}
+
+}
