986bc7c71e0a398b6b9f1c30c05bb1a0c921d6d6
kimu <kgc815@naver.com>
2019-05-23 21:30:37 +0900

    오류수정
    카테고리프로덕트로 접근해도 아이디 받아옴.
    오더리스트 productDAO 셀러이메일 주석처리함 (안하면 페이지 오류)

--- Sinseikai/src/net/member/action/MemberAddAction.java
+++ Sinseikai/src/net/member/action/MemberAddAction.java
@@ -30,7 +30,7 @@ public class MemberAddAction implements Action {
    				memberdata.setPw(request.getParameter("pw"));
    				memberdata.setEmail(request.getParameter("email"));
    				memberdata.setName(request.getParameter("name"));
-   				memberdata.setNum1(request.getParameter("num1"));
+   				memberdata.setNum1(Integer.parseInt(request.getParameter("num1")));
    				memberdata.setAge(age);
    				
    		   		result=memberdao.memberInsert(memberdata);
